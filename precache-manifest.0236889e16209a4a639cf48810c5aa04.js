018e60917d099fe3aaa5d2793085ff0f6a17dec9
AndroidDoctorr <gbartthenerd@gmai.com>
2020-01-10 01:41:38 -0500

    Updates

---
+++ precache-manifest.0236889e16209a4a639cf48810c5aa04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ff7bb43766e9548b9f29a877b20703",
+    "url": "/androiddoctorr.github.io/index.html"
+  },
+  {
+    "revision": "5434841d3271044b24b0",
+    "url": "/androiddoctorr.github.io/static/css/main.86e52b98.chunk.css"
+  },
+  {
+    "revision": "240723439beb29a8db6e",
+    "url": "/androiddoctorr.github.io/static/js/2.0da4e046.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/androiddoctorr.github.io/static/js/2.0da4e046.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5434841d3271044b24b0",
+    "url": "/androiddoctorr.github.io/static/js/main.731a2a54.chunk.js"
+  },
+  {
+    "revision": "afe62cf62ce874fb70bf",
+    "url": "/androiddoctorr.github.io/static/js/runtime-main.97ffcb40.js"
+  }
+]);
